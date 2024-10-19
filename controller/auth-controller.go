376af9dbfbd0c1addf0f0a4da27ece8a8e036b1e
egrizq <rizq.ramadhan17@gmail.com>
2024-10-19 08:04:56 +0700

    change later

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -11,7 +11,7 @@ import (
 func Login(c echo.Context) error {
 	res := &model.Response{
 		Status:  "Success",
-		Message: "Hellow from rizq",
+		Message: "Hellow good morning",
 	}
 
 	return c.JSON(http.StatusOK, res)
