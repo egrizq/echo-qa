95577200f3078596e01d63a385dc25253ecd8b82
egrizq <rizq.ramadhan17@gmail.com>
2024-10-19 09:45:02 +0700

    local repo

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -11,7 +11,7 @@ import (
 func Login(c echo.Context) error {
 	res := &model.Response{
 		Status:  "Success",
-		Message: "passing code",
+		Message: "checking newest code",
 	}
 
 	return c.JSON(http.StatusOK, res)
