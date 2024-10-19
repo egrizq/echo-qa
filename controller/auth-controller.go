f4a378e886e55c8fba6b00e69c489ccb9b32d4c8
egrizq <rizq.ramadhan17@gmail.com>
2024-10-19 07:57:47 +0700

    fix: hellow

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -11,7 +11,7 @@ import (
 func Login(c echo.Context) error {
 	res := &model.Response{
 		Status:  "Success",
-		Message: "Hellow from echo",
+		Message: "morning from echo",
 	}
 
 	return c.JSON(http.StatusOK, res)
